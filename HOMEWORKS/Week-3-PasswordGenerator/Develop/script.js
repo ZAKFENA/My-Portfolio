9fd35018a2a980bc6627d606de2ce7e3c2efdf44
Zekarias Negassa <zakfena@gmail.com>
2020-07-20 00:41:47 -0500

    worked on confirm, prompts and length criteria

--- HOMEWORKS/Week-3-PasswordGenerator/Develop/script.js
+++ HOMEWORKS/Week-3-PasswordGenerator/Develop/script.js
@@ -1,9 +1,63 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var numbers = ["0123456789"];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = [
   "!",
   "#",
@@ -47,7 +101,7 @@ function getRandomUpper() {
   return upperCase[Math.floor(Math.random() * upperCase.length)];
 }
 
-function getRandomnumbers() {
+function getRandomNumbers() {
   return numbers[Math.floor(Math.random() * numbers.length)];
 }
 
@@ -56,8 +110,42 @@ function getRandomCharacters() {
 }
 
 function generatePassword() {
-  return;
+  var lengthPrompt = prompt("How long do you want your password to be?");
+
+  if (lengthPrompt < 8 || lengthPrompt > 128 || isNaN(lengthPrompt)) {
+    alert("Please enter number between 8 and 128");
+    return "Please click Generate Button below";
+  }
+
+  // } else if (specialCharConfirm = true) {
+  //   getRandomLower(lowerCase).concat(
+  //     getRandomNumbers(numbers)
+  //   }
+  else
+    var specialCharConfirm = confirm(
+      "Do you want to have SPECIAL CHARACTER in your password?"
+    );
+  var lowerCaseConfirm = confirm(
+    "Do you want to have LOWER CASE in your password?"
+  );
+  var upperCaseConfirm = confirm(
+    "Do you want to have UPPER CASE in your password?"
+  );
+  var numberConfirm = confirm(
+     "Do you want to have NUMBER in your password?"
+  );
+
+  var 
+
+  for (var i = 0; i < lengthPrompt.length; i++) {
+    return getRandomLower(lowerCase).concat(
+      getRandomNumbers(numbers),
+      getRandomUpper(upperCase),
+      getRandomCharacters(specialChar)
+    );
+  }
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
